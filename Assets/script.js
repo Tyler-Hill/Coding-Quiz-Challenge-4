8913dd4b9ba318dc9adadee48ee548a1163d960a
Tyler <hilltyler49@gmail.com>
2022-11-08 18:25:56 -0600

    Added JS code, ensuring Git is connected

---
+++ Assets/script.js
@@ -0,0 +1,19 @@
+//global variables
+let startQuizButton = document.querySelector('#startQuiz');
+
+
+
+//functions
+function startQuiz() {
+    alert('I started the game')
+    //bring up question 1
+
+    //add four answer options
+    //make clickable
+}
+
+
+
+//function calls
+
+startQuizButton.addEventListener('click', startQuiz);
