8dfd5dce7c507b3c42bdc59496869d297e73c4c4
Tyler <hilltyler49@gmail.com>
2022-12-21 22:43:17 -0600

    began working on startQuiz function

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,9 @@
 //global variables
 let startQuizButton = document.querySelector("#startQuiz");
 let saveScore = document.querySelector("#saveInitials");
+let questionTitle = document.querySelector("#question");
+let buttonGrid = document.querySelector("#btnGrid");
+
 //let viewScores;
 const quizQuestions = [
   {
@@ -17,31 +20,31 @@ const quizQuestions = [
     choice2: "Choice 2 goes here.",
     choice3: "Choice 3 goes here.",
     choice4: "Choice 4 goes here.",
-    answer: "Choice 3 goes here.",
+    answer: "Choice 2 goes here.",
   },
   {
-    question: "Question 1 goes here.",
+    question: "Question 3 goes here.",
     choice1: "Choice 1 goes here.",
     choice2: "Choice 2 goes here.",
     choice3: "Choice 3 goes here.",
     choice4: "Choice 4 goes here.",
-    answer: "Choice 3 goes here.",
+    answer: "Choice 1 goes here.",
   },
   {
-    question: "Question 1 goes here.",
+    question: "Question 4 goes here.",
     choice1: "Choice 1 goes here.",
     choice2: "Choice 2 goes here.",
     choice3: "Choice 3 goes here.",
     choice4: "Choice 4 goes here.",
-    answer: "Choice 3 goes here.",
+    answer: "Choice 1 goes here.",
   },
   {
-    question: "Question 1 goes here.",
+    question: "Question 5 goes here.",
     choice1: "Choice 1 goes here.",
     choice2: "Choice 2 goes here.",
     choice3: "Choice 3 goes here.",
     choice4: "Choice 4 goes here.",
-    answer: "Choice 3 goes here.",
+    answer: "Choice 4 goes here.",
   },
 ];
 
@@ -49,11 +52,11 @@ const quizQuestions = [
 function startQuiz() {
   alert("I started the game");
   //bring up question 1
-  let title = document.createElement("h2");
-  title.textContent = "Who is the best magician?";
-  questionDiv.appendChild(title);
+  questionTitle.append(quizQuestions[0].question);
   //add four answer options
-  let btnOne = document.createElement("button");
+  let btnOne = quizQuestions[0].choice1;
+  let btnTwo = quizQuestions[0].choice2;
+  buttonGrid.append(btnOne, btnTwo);
 
   //make clickable
 }
